d3233a443ccff89d34d6d66b9638a0d831b7a4c7
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-01-30 15:56:47 +0000

    Can now create individual stacks with cube objects

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function() {
         console.log("addMenuListeners()");
         /* Toggles selected menu option value */
         $('.button-group').children().on('click', function() {
-            if(!$(this).hasClass('active')) {
+            if (!$(this).hasClass('active')) {
                 $(this).siblings().removeClass('active');
                 $(this).addClass('active');
             }
@@ -23,23 +23,53 @@ $(function() {
 
     /* Starts the game by taking the required settings */
     function startGame(noOfStacks, similarity, powered) {
-        stacksSetUp(noOfStacks);
+        console.log("startGame(" + noOfStacks + ", " + similarity + ", " + powered + ")");
+        var playerOneStacks = stacksSetUp(noOfStacks);
+        var playerTwoStacks;
+        if (similarity === 'EQUAL') {
+            playerTwoStacks = playerOneStacks;
+        } else {
+            playerTwoStacks = stacksSetUp(noOfStacks);
+        }
+
+        console.log("Player One: " + playerOneStacks);
+        console.log("Player Two: " + playerTwoStacks);
     }
 
     /* Sets up the game stacks */
     function stacksSetUp(noOfStacks) {
+        console.log("stacksSetUp()");
         var stackTypes = ['SINGLE', 'DOUBLE'];
         var stacks = [];
         for (var i = 0; i < stackTypes.indexOf(noOfStacks) + 1; i++) {
             stacks.push(createStack());
         }
-        console.log(stacks);
+        return stacks;
     }
 
     /* Creates a stack */
     function createStack() {
-        console.log("Stack Created");
-        return ['A', 'B'];
+        /* cube = {
+            color: 'color',
+            powerup: 'power-up'
+        } */
+        console.log("createStack()");
+        var stack = [];
+        var colors = ['Red', 'Blue', 'Green', 'Yellow'];
+        var colorMax = 3;
+        for (var i = 0; i < 10; i++) {
+            var newColor = colors[getRandom(0, colorMax)];
+            stack.push({
+                color: newColor,
+                powerup: 'none'
+            });
+        }
+        console.log(stack);
+        return stack;
+    }
+
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     /* Initialises web page */
