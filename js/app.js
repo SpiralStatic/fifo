06873d63869630251d87d1b0ec4057ec4f84214f
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-01-30 13:55:09 +0000

    Added play button listener that sends game settings through. Menu slides out

--- js/app.js
+++ js/app.js
@@ -9,6 +9,23 @@ $(function() {
                 $(this).addClass('active');
             }
         });
+
+        $('#play').on('click', function() {
+            var settings = $('#menu').find('.active');
+            startGame($(settings[0]).html(), $(settings[1]).html(), $(settings[2]).html());
+        });
+    }
+
+    function startGame(noOfStacks, similarity, powered) {
+        console.log(noOfStacks);
+        console.log(similarity);
+        console.log(powered);
+        $('#menu').slideUp();
+        $('#game').slideUp();
+    }
+
+    function stacksSetUp(noOfStacks) {
+
     }
 
     function initialise() {
