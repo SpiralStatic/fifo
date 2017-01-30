62c16cf3b99f7696e4144f6fca8907ec1af9fb1e
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-01-30 13:06:12 +0000

    Added gradient effects, background image, menu event listeners

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation();
+$(function() {
+    $(document).foundation();
+
+    function addMenuListeners() {
+        console.log("addMenuListeners()");
+        $('.button-group').children().on('click', function() {
+            if(!$(this).hasClass('active')) {
+                $(this).siblings().removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+    }
+
+    function initialise() {
+        addMenuListeners();
+    }
+
+    initialise();
+});
