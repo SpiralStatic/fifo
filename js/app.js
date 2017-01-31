b3354df9e82a6d38ab794687eb06d6bc8cae282d
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-01-31 12:29:17 +0000

    Refactored code block into OOP version

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-$(() => {
-    $(document).foundation();
+$(document).foundation();
 
-    /* Variables */
-    var colors = ['red', 'blue', 'green', 'yellow'];
-    var playerOneStacks = [];
-    var playerTwoStacks = [];
-    var stackType;
-    var playerOnePoints = 0;
-    var playerTwoPoints = 0;
+class FIFO {
+    constructor() {
+        /* Variables */
+        this.colors = ['red', 'blue', 'green', 'yellow'];
+        this.playerOneStacks = [];
+        this.playerTwoStacks = [];
+        this.stackType = 'Single';
+        this.playerOnePoints = 0;
+        this.playerTwoPoints = 0;
+
+        this.addMenuListeners();
+    }
 
     /* Adds the game menu listeners to the options */
-    function addMenuListeners() {
+    addMenuListeners() {
         console.log("addMenuListeners()");
         /* Toggles selected menu option value */
         $('.button-group').children().on('click', (event) => {
@@ -24,61 +28,61 @@ $(() => {
         /* When play is pressed, gets the menu option values, then switches to game */
         $('#game').hide();
         $('#play').on('click', () => {
-            var settings = $('#menu').find('.active');
-            startGame($(settings[0]).html(), $(settings[1]).html(), $(settings[2]).html());
+            let settings = $('#menu').find('.active');
+            this.startGame($(settings[0]).html(), $(settings[1]).html(), $(settings[2]).html());
             $('#menu').slideToggle('slow');
             $('#game').slideToggle('slow');
-            addGameListeners();
+            this.addGameListeners();
         });
     }
 
     /* Adds the listeners for the game keys that are to be used */
-    function addGameListeners() {
+    addGameListeners() {
         // Q - 113; A - 97; S - 115; W - 119;
         // I - 105; K - 107; L - 108; O - 111;
-        $(document).bind('keypress', function(e) {
-            keyCheck(e.keyCode);
+        $(document).bind('keypress', (event) => {
+            this.keyCheck(event.keyCode);
         });
     }
 
     /* Starts the game by taking the required settings */
-    function startGame(noOfStacks, similarity, powered) {
+    startGame(noOfStacks, similarity, powered) {
         console.log("startGame(" + noOfStacks + ", " + similarity + ", " + powered + ")");
-        playerOneStacks = stacksSetUp(noOfStacks);
+        this.playerOneStacks = this.stacksSetUp(noOfStacks);
         if (similarity === 'EQUAL') {
-            playerTwoStacks = copyArray(playerOneStacks);
+            this.playerTwoStacks = this.copyArray(this.playerOneStacks);
         } else {
-            playerTwoStacks = stacksSetUp(noOfStacks);
+            this.playerTwoStacks = this.stacksSetUp(noOfStacks);
         }
-        playerOneStacks.player = 'one';
-        playerTwoStacks.player = 'two';
-        displayStacks('one', playerOneStacks);
-        displayStacks('two', playerTwoStacks);
+        this.playerOneStacks.player = 'one';
+        this.playerTwoStacks.player = 'two';
+        this.displayStacks('one', this.playerOneStacks);
+        this.displayStacks('two', this.playerTwoStacks);
     }
 
     /* Sets up the game stacks */
-    function stacksSetUp(noOfStacks) {
+    stacksSetUp(noOfStacks) {
         console.log("stacksSetUp(" + noOfStacks + ")");
-        var stackTypes = ['SINGLE', 'DOUBLE'];
-        stackType = noOfStacks;
-        var stacks = [];
-        for (var i = 0; i < stackTypes.indexOf(noOfStacks) + 1; i++) {
-            stacks.push(createStack());
+        let stackTypes = ['SINGLE', 'DOUBLE'];
+        this.stackType = noOfStacks;
+        let stacks = [];
+        for (let i = 0; i < stackTypes.indexOf(noOfStacks) + 1; i++) {
+            stacks.push(this.createStack());
         }
         return stacks;
     }
 
     /* Creates a stack full of cube objects and returns it*/
-    function createStack() {
+    createStack() {
         /* cube = {
             color: 'color',
             powerup: 'power-up'
         } */
         console.log("createStack()");
-        var stack = [];
-        var colorMax = 3;
-        for (var i = 0; i < 20; i++) {
-            var newColor = colors[getRandom(0, colorMax)];
+        let stack = [];
+        let colorMax = 3;
+        for (let i = 0; i < 200; i++) {
+            let newColor = this.colors[this.getRandom(0, colorMax)];
             stack.push({
                 color: newColor,
                 powerup: 'none'
@@ -89,22 +93,22 @@ $(() => {
     }
 
     /* Returns a random value between the min-max parameters */
-    function getRandom(min, max) {
+    getRandom(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     /* Copies over an array by parsing its objects to a new array */
-    function copyArray(original) {
+    copyArray(original) {
         return JSON.parse(JSON.stringify(original));
     }
 
     /* Outputs and displays the cubes on the users screen up-to the choosen value */
-    function displayStacks(playerNo, playerStacks) {
+    displayStacks(playerNo, playerStacks) {
         console.log("displayStacks(" + playerNo + ", " + playerStacks + ")");
-        var maxCubes = 7;
-        for (var i = 0; i < playerStacks.length; i++) {
+        let maxCubes = 7;
+        for (let i = 0; i < playerStacks.length; i++) {
             $('#player-' + playerNo).append('<ul id="' + playerNo + i + '">');
-            for (var j = 0; j < maxCubes; j++) {
+            for (let j = 0; j < maxCubes; j++) {
                 $('ul#' + playerNo + i).prepend('<li class="cube ' + playerStacks[i][j].color + ' ' + playerStacks[i][j].powerup + '"></li>');
             }
             $('#player-' + playerNo).append('</ul>');
@@ -112,33 +116,33 @@ $(() => {
     }
 
     /* Upon key press triggers function to check key to cube */
-    function keyCheck(keyPress) {
+    keyCheck(keyPress) {
         // Q - 113; W - 119 ; A - 97; S - 115;
         switch (keyPress) {
             case 113:
-                canRemoveCube(playerOneStacks, colors[0]); // Red
+                this.canRemoveCube(this.playerOneStacks, this.colors[0]); // Red
                 break;
             case 97:
-                canRemoveCube(playerOneStacks, colors[1]); // Blue
+                this.canRemoveCube(this.playerOneStacks, this.colors[1]); // Blue
                 break;
             case 119:
-                canRemoveCube(playerOneStacks, colors[2]); // Green
+                this.canRemoveCube(this.playerOneStacks, this.colors[2]); // Green
                 break;
             case 115:
-                canRemoveCube(playerOneStacks, colors[3]); // Yellow
+                this.canRemoveCube(this.playerOneStacks, this.colors[3]); // Yellow
                 break;
                 // I - 105; O - 111; K - 107; L - 108;
             case 105:
-                canRemoveCube(playerTwoStacks, colors[0]); // Red
+                this.canRemoveCube(this.playerTwoStacks, this.colors[0]); // Red
                 break;
             case 107:
-                canRemoveCube(playerTwoStacks, colors[1]); // Blue
+                this.canRemoveCube(this.playerTwoStacks, this.colors[1]); // Blue
                 break;
             case 111:
-                canRemoveCube(playerTwoStacks, colors[2]); // Green
+                this.canRemoveCube(this.playerTwoStacks, this.colors[2]); // Green
                 break;
             case 108:
-                canRemoveCube(playerTwoStacks, colors[3]); // Yellow
+                this.canRemoveCube(this.playerTwoStacks, this.colors[3]); // Yellow
                 break;
             default:
                 break;
@@ -146,44 +150,44 @@ $(() => {
     }
 
     /* Checks if cube is same, and if so triggers removal and display update */
-    function canRemoveCube(playerStack, color) {
-        if (stackType === 'DOUBLE' && playerStack[0][0].color === color && playerStack[1][0].color === color) {
-            removeCube(playerStack[0]);
-            removeCube(playerStack[1]);
-            updateDisplay(playerStack[0]);
-            updateDisplay(playerStack[1]);
-            addPoints(playerStack.player);
-            addPoints(playerStack.player);
+    canRemoveCube(playerStack, color) {
+        if (this.stackType === 'DOUBLE' && playerStack[0][0].color === color && playerStack[1][0].color === color) {
+            this.removeCube(playerStack[0]);
+            this.removeCube(playerStack[1]);
+            this.updateDisplay(playerStack[0]);
+            this.updateDisplay(playerStack[1]);
+            this.addPoints(playerStack.player);
+            this.addPoints(playerStack.player);
         } else if (playerStack[0][0].color === color) {
-            removeCube(playerStack[0]);
-            updateDisplay(playerStack[0]);
-            addPoints(playerStack.player);
-        } else if (stackType === 'DOUBLE' && playerStack[1][0].color === color) {
-            removeCube(playerStack[1]);
-            updateDisplay(playerStack[1]);
-            addPoints(playerStack.player);
+            this.removeCube(playerStack[0]);
+            this.updateDisplay(playerStack[0]);
+            this.addPoints(playerStack.player);
+        } else if (this.stackType === 'DOUBLE' && playerStack[1][0].color === color) {
+            this.removeCube(playerStack[1]);
+            this.updateDisplay(playerStack[1]);
+            this.addPoints(playerStack.player);
         }
-        console.log("one: " + playerOnePoints);
-        console.log("two: " + playerTwoPoints);
+        console.log("one: " + this.playerOnePoints);
+        console.log("two: " + this.playerTwoPoints);
     }
 
     // Removes a single cube from the front of an array */
-    function removeCube(playerStack) {
+    removeCube(playerStack) {
         //console.log(playerStack);
         playerStack.shift();
     }
 
     /* Updates the game display when cube is removed */
-    function updateDisplay(playerStack) {
+    updateDisplay(playerStack) {
         var getCorrectHTMLStack;
 
-        if (playerStack === playerOneStacks[0]) {
+        if (playerStack === this.playerOneStacks[0]) {
             getCorrectHTMLStack = 'ul#one0';
-        } else if (playerStack === playerOneStacks[1]) {
+        } else if (playerStack === this.playerOneStacks[1]) {
             getCorrectHTMLStack = 'ul#one1';
-        } else if (playerStack === playerTwoStacks[0]) {
+        } else if (playerStack === this.playerTwoStacks[0]) {
             getCorrectHTMLStack = 'ul#two0';
-        } else if (playerStack === playerTwoStacks[1]) {
+        } else if (playerStack === this.playerTwoStacks[1]) {
             getCorrectHTMLStack = 'ul#two1';
         }
         //console.log(getCorrectHTMLStack);
@@ -192,22 +196,24 @@ $(() => {
     }
 
     /* Increments the players point value */
-    function addPoints(player) {
-        if(player === 'one') {
-            playerOnePoints++;
-        }else if(player === 'two') {
-            playerTwoPoints++;
+    addPoints(player) {
+        if (player === 'one') {
+            this.playerOnePoints++;
+        } else if (player === 'two') {
+            this.playerTwoPoints++;
         }
     }
+}
 
-    /* Initialises web page */
-    function initialise() {
-        addMenuListeners();
+class Player {
+    constructor() {
+        this.stacks = [];
     }
+}
 
-    initialise();
+$(() => {
+    const fifo = new FIFO();
 });
-
 
 // Change all vars to lets or consts
 // Change all anonymous functions to arrow syntax
